89335a84d55ef006445e157c6324200eb62171aa
Abhishek Bhatt <abhishekbhatt9011@gmail.com>
2026-02-04 12:47:01 +0530

    biz_finance_ops env

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 import express, { type Request, Response, NextFunction } from "express";
-import { registerRoutes } from "./routes.js";
+import { registerRoutes } from "./routes";
 import { createServer } from "http";
 import path from "path";
 import { fileURLToPath } from "url";
